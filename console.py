a3f6be00cb3b7026b0ac1cb044d2e8484bb975a5
Ronald Abimbola <abimbolaaderinsola212@gmail.com>
2024-01-10 18:02:16 +0200

    Modified: Refactored the file docs and created do_show method

--- console.py
+++ console.py
@@ -1,27 +1,33 @@
 #!/usr/bin/python3
 """This module contains the entry point of the command interpreter"""
 
-
 import cmd
 import models
 
 class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
-    ruler = "="
+
     
     def do_quit(self, arg):
         """
         Quit the console.
 
-        This method is called when the user enters the 'quit' command in the console.
+        Usage:
+            quit
+            q
+            exit
         """
         return True
-
+    
+    do_q = do_quit
+    do_exit = do_quit
+    
     def do_EOF(self, arg):
         """
         Quit the console.
 
-        This method is called when the user enters the 'EOF' command in the console.
+        Usage:
+            EOF
         """        
         return True
     
@@ -34,6 +40,11 @@ class HBNBCommand(cmd.Cmd):
     def do_create(self, arg):
         """
         Creates a new inst of BaseModel, saves it (to the JSON file) and prints the id.
+        
+        Usage: create <class_name>
+        
+        Args:
+            arg (str): The argument should contain the <class_name>.
         """
         
         class_name = arg.split(" ")[0]
@@ -46,6 +57,33 @@ class HBNBCommand(cmd.Cmd):
             instance = models.classes[class_name]()
             instance.save()
             print(instance.id)
+    
+    def do_show(self, arg):
+        """
+        Displays the string representation of an instance.
+        
+        Usage: create <class_name> <instance_id>
+        
+        Args:
+            arg (str): The argument should contain <class_name> and <instance_id>.
+        """
+        
+        split_args = arg.split(" ")
+        class_name = split_args[0] if len(split_args) > 0 else None
+        obj_id = split_args[1] if len(split_args) > 1 else None
+        
+        if not class_name:
+            print("** class name missing **")
+        elif class_name not in models.classes:
+            print("** class doesn't exist **")
+        elif not obj_id:
+            print("** instance id missing **")
+        else:
+            key = f"{class_name}.{obj_id}"
+            if key in models.loaded_objects:
+                print(models.loaded_objects[key])
+            else:
+                print("** no instance found **")
         
 
 if __name__ == '__main__':
