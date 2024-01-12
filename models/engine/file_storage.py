f1af3814122d3073e7d187fd556b45f7a30cfe1e
Ronald Abimbola <abimbolaaderinsola212@gmail.com>
2024-01-12 11:37:49 +0200

    Modified: Configured `FileStorage` to work with the new classes

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -7,6 +7,11 @@ and retrieves objects to and from a JSON file.
 import json
 from models.base_model import BaseModel
 from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 
 class FileStorage:
