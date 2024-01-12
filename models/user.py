f08bee3a61e5546e2753870274f5eaa405ddcc8c
Ronald Abimbola <abimbolaaderinsola212@gmail.com>
2024-01-12 10:59:19 +0200

    Created: I created the User class

---
+++ models/user.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+This module contains the User class (Blueprint for creating user objects).
+"""
+
+from models.base_model import BaseModel
+
+class User(BaseModel):
+    """
+    This is the user class.
+
+    Attributes:
+        email (str): The email address of the user.
+        password (str): The password of the user.
+        first_name (str): The first name of the user.
+        last_name (str): The last name of the user.
+    """
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
+    
+    def __init__(self, *args, **kwargs):
+        """
+        Initialize public instance attributes.
+        """
+        super().__init__(*args, **kwargs)
+        
+        self.email = User.email
+        self.password = User.password
+        self.first_name = User.first_name
+        self.last_name = User.last_name
